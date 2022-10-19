286cff19b30184f8e33dd1196cde366b24dca060
HarshilPatel02 <harshilpatelld@gmail.com>
2022-10-19 22:12:41 +0530

    LongestPalindrome

--- Longest_Palindrome_substring.py
+++ Longest_Palindrome_substring.py
@@ -1,14 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         length = len(s)
+        temp = s
 
-        rev_s = s[::-1]
+        for i in range(length, 1, -1):
+            ini = 0
 
-        if s.startswith(rev_s[:length//2 - 1]):
-            return s
-        else:
-            return False
+            while ini + i <= length:
+
+                s = temp[ini:ini + i]
+
+                rev_s = s[::-1]
+
+                if s.startswith(rev_s):
+                    return rev_s
+                ini += 1
+
+        return s[0]
 
 
 objt = Solution()
-print(objt.longestPalindrome("abbab"))
+print(objt.longestPalindrome("absdfddsdf"))
